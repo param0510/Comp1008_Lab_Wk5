0850dc1aed9f58a847be4355c84d5ae8c27a9682
param0510 <paramveer0510@gmail.com>
2022-02-14 23:36:32 -0500

    Question 2.1 complete

--- src/Phone.java
+++ src/Phone.java
@@ -1,2 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Phone {
+    private String make, model;
+    private int memory;
+    private double price;
+
+    public Phone(String make, String model, int memory, double price) {
+        setMake(make);
+        setModel(model);
+        setMemory(memory);
+        setPrice(price);
+    }
 }
