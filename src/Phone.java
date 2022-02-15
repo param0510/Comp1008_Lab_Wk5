01a32e6150174c4356f843a3ef23f75526fd568a
param0510 <paramveer0510@gmail.com>
2022-02-14 23:42:07 -0500

    Question 2.2 complete

--- src/Phone.java
+++ src/Phone.java
@@ -12,4 +12,13 @@ public class Phone {
         setMemory(memory);
         setPrice(price);
     }
+
+    public void setModel(String model) {
+        if (make.length() >= 3) {
+            this.model = model;
+        }else{
+            throw new IllegalArgumentException(model + " is not a valid model name, as it is less than 3 characters long!");
+        }
+
+    }
 }
